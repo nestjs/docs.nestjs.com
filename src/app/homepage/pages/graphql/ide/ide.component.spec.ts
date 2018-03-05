89c9aad8ae70e1f80378bcf0ee00ed9c9c2b9035
thomrick <tricart@xebia.fr>
2018-03-05 21:37:29 +0100

    #cli : fix ide component unit tests

--- src/app/homepage/pages/graphql/ide/ide.component.spec.ts
+++ src/app/homepage/pages/graphql/ide/ide.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IdeComponent } from './ide.component';
+import { TabsComponent } from '../../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../../shared/pipes/extension.pipe';
 
 describe('IdeComponent', () => {
-  let component: IdeComponent;
-  let fixture: ComponentFixture<IdeComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IdeComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        IdeComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<IdeComponent>;
+  let component: IdeComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(IdeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
