a3a1e4e23804c35233e9c6a1b680f0597ed26140
thomrick <tricart@xebia.fr>
2018-03-05 18:51:40 +0100

    #cli : fix footer unit tests

--- src/app/homepage/footer/footer.component.spec.ts
+++ src/app/homepage/footer/footer.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ FooterComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<FooterComponent>;
+  let component: FooterComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
