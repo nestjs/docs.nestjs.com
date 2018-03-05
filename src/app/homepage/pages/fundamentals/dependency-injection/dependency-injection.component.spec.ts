25a714d3a7dbab68b01f44e046346305b43a408f
thomrick <tricart@xebia.fr>
2018-03-05 21:31:13 +0100

    #cli : fix dependency-injection component unit tests

--- src/app/homepage/pages/fundamentals/dependency-injection/dependency-injection.component.spec.ts
+++ src/app/homepage/pages/fundamentals/dependency-injection/dependency-injection.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DependencyInjectionComponent } from './dependency-injection.component';
+import { TabsComponent } from '../../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../../shared/pipes/extension.pipe';
 
 describe('DependencyInjectionComponent', () => {
-  let component: DependencyInjectionComponent;
-  let fixture: ComponentFixture<DependencyInjectionComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DependencyInjectionComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        DependencyInjectionComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<DependencyInjectionComponent>;
+  let component: DependencyInjectionComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(DependencyInjectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
