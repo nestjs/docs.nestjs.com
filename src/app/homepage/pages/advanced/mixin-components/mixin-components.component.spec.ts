963d3c07c49c15a3fd020bdfefde911f26dab178
thomrick <tricart@xebia.fr>
2018-03-05 19:06:39 +0100

    #cli : fix mixin-components component unit tests

--- src/app/homepage/pages/advanced/mixin-components/mixin-components.component.spec.ts
+++ src/app/homepage/pages/advanced/mixin-components/mixin-components.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MixinComponentsComponent } from './mixin-components.component';
 
 describe('MixinComponentsComponent', () => {
-  let component: MixinComponentsComponent;
-  let fixture: ComponentFixture<MixinComponentsComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ MixinComponentsComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<MixinComponentsComponent>;
+  let component: MixinComponentsComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(MixinComponentsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
