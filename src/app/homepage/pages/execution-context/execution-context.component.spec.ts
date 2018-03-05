95afb3d6a39b3d1e9aec0b618295a76557366aa8
thomrick <tricart@xebia.fr>
2018-03-05 19:14:44 +0100

    #cli : fix execution-context component unit tests

--- src/app/homepage/pages/execution-context/execution-context.component.spec.ts
+++ src/app/homepage/pages/execution-context/execution-context.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExecutionContextComponent } from './execution-context.component';
+import { TabsComponent } from '../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../shared/pipes/extension.pipe';
 
 describe('ExecutionContextComponent', () => {
-  let component: ExecutionContextComponent;
-  let fixture: ComponentFixture<ExecutionContextComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExecutionContextComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        ExecutionContextComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<ExecutionContextComponent>;
+  let component: ExecutionContextComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(ExecutionContextComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
