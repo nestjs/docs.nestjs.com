2e9e83c96fc794f582cac3840bb7ff4c2fa485d2
thomrick <tricart@xebia.fr>
2018-03-05 19:02:42 +0100

    #cli : fail to fix menu component unit tests caused by sent request

--- src/app/homepage/menu/menu.component.spec.ts
+++ src/app/homepage/menu/menu.component.spec.ts
@@ -1,18 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+    return TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        MenuComponent,
+        MenuItemComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<MenuComponent>;
+  let component: MenuComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
