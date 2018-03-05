b01d438e87d81293e51e6c39fc0bb6e1867c3c0c
thomrick <tricart@xebia.fr>
2018-03-05 18:52:41 +0100

    #cli : fix header component unit tests

--- src/app/homepage/header/header.component.spec.ts
+++ src/app/homepage/header/header.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<HeaderComponent>;
+  let component: HeaderComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
