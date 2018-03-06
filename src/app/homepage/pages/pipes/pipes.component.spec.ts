9efeec83f80d644f24133098bdaaa741853dd04e
thomrick <tricart@xebia.fr>
2018-03-06 09:01:06 +0100

    #cli : fix pipes component unit tests

--- src/app/homepage/pages/pipes/pipes.component.spec.ts
+++ src/app/homepage/pages/pipes/pipes.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PipesComponent } from './pipes.component';
+import { TabsComponent } from '../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../shared/pipes/extension.pipe';
 
 describe('PipesComponent', () => {
-  let component: PipesComponent;
-  let fixture: ComponentFixture<PipesComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PipesComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        PipesComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<PipesComponent>;
+  let component: PipesComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(PipesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
