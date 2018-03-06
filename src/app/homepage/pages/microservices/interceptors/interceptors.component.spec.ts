be2d39a545365ddfec9c360d788cb0253a0e64c7
thomrick <tricart@xebia.fr>
2018-03-06 08:55:40 +0100

    #cli : fix interceptors component unit tests

--- src/app/homepage/pages/microservices/interceptors/interceptors.component.spec.ts
+++ src/app/homepage/pages/microservices/interceptors/interceptors.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MicroservicesInterceptorsComponent } from './interceptors.component';
 
 describe('MicroservicesInterceptorsComponent', () => {
-  let component: MicroservicesInterceptorsComponent;
-  let fixture: ComponentFixture<MicroservicesInterceptorsComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ MicroservicesInterceptorsComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<MicroservicesInterceptorsComponent>;
+  let component: MicroservicesInterceptorsComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(MicroservicesInterceptorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
