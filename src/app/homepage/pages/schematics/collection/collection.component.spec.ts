1647dabf806636359b341de8d85f03a80f70b868
thomrick <tricart@xebia.fr>
2018-03-06 09:19:48 +0100

    #cli : fix collection component unit tests

--- src/app/homepage/pages/schematics/collection/collection.component.spec.ts
+++ src/app/homepage/pages/schematics/collection/collection.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SchematicsCollectionComponent } from './collection.component';
 
 describe('SchematicsCollectionComponent', () => {
-  let component: SchematicsCollectionComponent;
-  let fixture: ComponentFixture<SchematicsCollectionComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ SchematicsCollectionComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<SchematicsCollectionComponent>;
+  let component: SchematicsCollectionComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(SchematicsCollectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
