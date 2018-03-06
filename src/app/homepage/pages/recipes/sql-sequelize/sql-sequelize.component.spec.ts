44a6b2624aa06c83e6086e3d56b530e3b506690c
thomrick <tricart@xebia.fr>
2018-03-06 09:09:49 +0100

    #cli : fix sql-sequelize component unit tests

--- src/app/homepage/pages/recipes/sql-sequelize/sql-sequelize.component.spec.ts
+++ src/app/homepage/pages/recipes/sql-sequelize/sql-sequelize.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SqlSequelizeComponent } from './sql-sequelize.component';
 
 describe('SqlSequelizeComponent', () => {
-  let component: SqlSequelizeComponent;
-  let fixture: ComponentFixture<SqlSequelizeComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ SqlSequelizeComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<SqlSequelizeComponent>;
+  let component: SqlSequelizeComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(SqlSequelizeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
