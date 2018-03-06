a046e55687c9f4c2429e6f87b21c91c8ab745554
thomrick <tricart@xebia.fr>
2018-03-06 09:23:37 +0100

    #cli : fix mongo component unit tests

--- src/app/homepage/pages/techniques/mongo/mongo.component.spec.ts
+++ src/app/homepage/pages/techniques/mongo/mongo.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MongoComponent } from './mongo.component';
+import { TabsComponent } from '../../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../../shared/pipes/extension.pipe';
 
 describe('MongoComponent', () => {
-  let component: MongoComponent;
-  let fixture: ComponentFixture<MongoComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MongoComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        MongoComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<MongoComponent>;
+  let component: MongoComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(MongoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
