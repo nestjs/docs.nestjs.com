6b1502839e717522fb41cabddee4cea3135b6cbc
thomrick <tricart@xebia.fr>
2018-03-06 09:39:15 +0100

    #cli : fix tabs component unit tests

--- src/app/shared/components/tabs/tabs.component.spec.ts
+++ src/app/shared/components/tabs/tabs.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabsComponent } from './tabs.component';
 
 describe('TabsComponent', () => {
-  let component: TabsComponent;
-  let fixture: ComponentFixture<TabsComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [ TabsComponent ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<TabsComponent>;
+  let component: TabsComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
