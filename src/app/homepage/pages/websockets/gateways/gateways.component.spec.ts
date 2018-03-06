b4c3ed73d247e31fa0433bb7d4bced4add158246
thomrick <tricart@xebia.fr>
2018-03-06 09:33:48 +0100

    #cli : fix gateways component unit tests

--- src/app/homepage/pages/websockets/gateways/gateways.component.spec.ts
+++ src/app/homepage/pages/websockets/gateways/gateways.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GatewaysComponent } from './gateways.component';
+import { TabsComponent } from '../../../../shared/components/tabs/tabs.component';
+import { ExtensionPipe } from '../../../../shared/pipes/extension.pipe';
 
 describe('GatewaysComponent', () => {
-  let component: GatewaysComponent;
-  let fixture: ComponentFixture<GatewaysComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GatewaysComponent ]
+    return TestBed.configureTestingModule({
+      declarations: [
+        TabsComponent,
+        ExtensionPipe,
+        GatewaysComponent
+      ]
     })
     .compileComponents();
   }));
 
+  let fixture: ComponentFixture<GatewaysComponent>;
+  let component: GatewaysComponent;
   beforeEach(() => {
     fixture = TestBed.createComponent(GatewaysComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
