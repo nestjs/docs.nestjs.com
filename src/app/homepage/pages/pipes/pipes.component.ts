5ba586ae065ff150dcd456619b429ed34bf86fe0
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2018-05-10 13:27:38 +0700

    Async removed in JS

--- src/app/homepage/pages/pipes/pipes.component.ts
+++ src/app/homepage/pages/pipes/pipes.component.ts
@@ -234,7 +234,7 @@ import { Injectable, BadRequestException} from '@nestjs/common';
 
 @Injectable()
 export class ParseIntPipe {
-  async transform(value, metadata) {
+  transform(value, metadata) {
     const val = parseInt(value, 10);
     if (isNaN(val)) {
       throw new BadRequestException('Validation failed');
