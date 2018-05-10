8afcd4cb786adb65cf0ba2420a659b5a1d2db759
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2018-05-10 13:08:43 +0700

    Fixed ParseIntPipe

--- src/app/homepage/pages/pipes/pipes.component.ts
+++ src/app/homepage/pages/pipes/pipes.component.ts
@@ -218,7 +218,7 @@ import { PipeTransform, Injectable, ArgumentMetadata, HttpStatus, BadRequestExce
 
 @Injectable()
 export class ParseIntPipe implements PipeTransform<string, number> {
-  async transform(value: string, metadata: ArgumentMetadata): number {
+  transform(value: string, metadata: ArgumentMetadata): number {
     const val = parseInt(value, 10);
     if (isNaN(val)) {
       throw new BadRequestException('Validation failed');
