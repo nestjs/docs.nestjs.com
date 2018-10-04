fdcf0850b9b6269960bc7a46ab0957b6adb936bf
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-10-04 16:46:05 +0200

    seo: update title and metatags for better seo

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
 export const HOMEPAGE_TITLE = 'Documentation';
-export const TITLE_SUFFIX = ' | Nest - A progressive Node.js web framework';
+export const TITLE_SUFFIX = ' | NestJS - A progressive Node.js web framework';
