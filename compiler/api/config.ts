60967fa8393b289d97b72a05f4b658147d870bf1
Livio <livio.brunner.lb1@gmail.com>
2019-06-24 23:20:53 +0200

    feat(api): Add passport module

--- compiler/api/config.ts
+++ compiler/api/config.ts
@@ -7,5 +7,6 @@ export const OUTPUT_PATH = resolve(__dirname, '../../src/generated/docs/api');
 export const PACKAGES_PATH: string[] = [
   './nest/packages/core/index.ts',
   './nest/packages/common/index.ts',
-  './terminus/lib/index.ts'
+  './terminus/lib/index.ts',
+  './passport/lib/index.ts',
 ];
