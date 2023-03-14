eb68dc663c5af0967c77a8f12c4eed72271f77c2
SocketSomeone <socket.someone@gmail.com>
2023-03-14 20:59:24 +0300

    revert: usage spaces instead of tabs

--- src/app/homepage/pages/recipes/necord/necord.component.ts
+++ src/app/homepage/pages/recipes/necord/necord.component.ts
@@ -2,8 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BasePageComponent } from '../../page/page.component';
 
 @Component({
-	selector: 'app-necord',
-	templateUrl: './necord.component.html',
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-necord',
+  templateUrl: './necord.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NecordComponent extends BasePageComponent {}
