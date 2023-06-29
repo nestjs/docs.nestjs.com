2a799e9fcbb6953bd7c444881a581f1cae6b2bad
Tony133 <a.tripodi133@gmail.com>
2023-06-29 16:34:59 +0200

    chore(): improvements dark mode

--- src/app/homepage/dark-mode-toggle/dark-mode-toggle.component.ts
+++ src/app/homepage/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,5 +1,6 @@
-import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-dark-mode-toggle',
@@ -9,8 +10,29 @@ import { Component, Inject, OnInit } from '@angular/core';
 export class DarkModeToggleComponent implements OnInit {
   isDarkMode: boolean;
 
+  constructor(
+    @Inject(DOCUMENT)
+    private readonly document: Document,
+    private readonly mediaMatcher: MediaMatcher,
+  ) {}
+
+  ngOnInit() {
+    const userPrefersDark =
+      this.mediaMatcher.matchMedia &&
+      this.mediaMatcher.matchMedia('(prefers-color-scheme: dark)').matches;
+    // In case the user has used the toggle button, we prioritize it over the
+    // system settings
+    this.setDarkMode(this.getUserSettingsIsDarkMode() || userPrefersDark);
+  }
+
+  toggleDarkMode() {
+    const isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme-mode', isDarkMode.toString());
+    this.setDarkMode(isDarkMode);
+  }
+
   private getUserSettingsIsDarkMode(): boolean {
-    return localStorage.getItem('DARK_MODE') === 'true';
+    return localStorage.getItem('theme-mode') === 'true';
   }
 
   private setDarkMode(isDarkMode: boolean) {
@@ -19,22 +41,5 @@ export class DarkModeToggleComponent implements OnInit {
       'mode',
       isDarkMode ? 'dark' : 'light',
     );
-  }
-
-  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
-
-  ngOnInit() {
-    const userPrefersDark =
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches;
-    // In case the user has used the toggle button, we prioritize it over the
-    // system settings
-    this.setDarkMode(this.getUserSettingsIsDarkMode() || userPrefersDark);
-  }
-
-  toggleDarkMode() {
-    const isDarkMode = !this.isDarkMode;
-    localStorage.setItem('DARK_MODE', isDarkMode.toString());
-    this.setDarkMode(isDarkMode);
   }
 }
