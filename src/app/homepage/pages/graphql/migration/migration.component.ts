5e80bcbbac1e76beb5c27e431775c1ba78f84d35
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-17 15:01:18 +0100

    chore: remove outdated migration pages, push v10 docs copy

--- src/app/homepage/pages/graphql/migration/migration.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BasePageComponent } from '../../page/page.component';
-
-@Component({
-  selector: 'app-migration',
-  templateUrl: './migration.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class MigrationComponent extends BasePageComponent {}
