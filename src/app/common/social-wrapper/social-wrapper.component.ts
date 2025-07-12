3ab99bd1fd3f1b1fdfd01a3921b2ac3ee7ae89b6
Dipanshu Rawat <dv451197@gmail.com>
2025-07-13 00:50:32 +0530

    fix: Show GitHub stars on social wrapper

--- src/app/common/social-wrapper/social-wrapper.component.ts
+++ src/app/common/social-wrapper/social-wrapper.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-social-wrapper',
   templateUrl: './social-wrapper.component.html',
-  styleUrls: ['./social-wrapper.component.scss']
+  styleUrls: ['./social-wrapper.component.scss'],
 })
-export class SocialWrapperComponent {}
+export class SocialWrapperComponent implements OnInit {
+  githubStars: string = '';
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    const owner = 'nestjs';
+    const repo = 'nest';
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}`;
+
+    this.http.get<any>(apiUrl).subscribe({
+      next: (data) => {
+        const stars = data.stargazers_count;
+        this.githubStars = this.formatStars(stars);
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('GitHub API error:', err);
+      },
+    });
+  }
+
+  private formatStars(count: number): string {
+    if (count >= 1_000_000) {
+      return (count / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
+    } else if (count >= 1_000) {
+      return (count / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
+    } else {
+      return count.toString();
+    }
+  }
+}
